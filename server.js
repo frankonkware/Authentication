474dcc6efdbf75f208e57a800a0d2db8fc551211
frankonkware <frankonkware@gmail.com>
2020-03-18 16:50:28 +0300

    homepage route added

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 const express = require('express');
+const uuid    = require('uuid/v4');
+const session = require('express-session');
+
 
 //create the server
 const app = express();
 
 
+//add and configure middleware
+app.use(session({
+    genid: (req) => {
+        console.log('Inside the session middleware')
+    }
+}))
 
 //HOme page route
 app.get('/', function(req,res){
-    res.send("You just Hit the home page");
+    console.log(req);
+    const uniqueId = uuid();    
+    res.send(`Home page hit. Recieved the unique id: ${uniqueId}`);
 });
 
 
